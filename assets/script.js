8db75c40e38715a5f796d1e5bb84ba5f79b7cd0f
Minh Y <le.minh.y.95@gmail.com>
2021-08-28 19:27:58 +0700

    add-script

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+const buyTicketBtns = document.querySelectorAll('.buy-ticket-btn')
+const popUp = document.querySelector('.modal')
+// const modalContainer = document.querySelector('.modal-container')
+const closeBtn1 = document.querySelector('.close-modal')
+const closeBtn2 = document.querySelector('.close-btn')
+
+buyTicketBtns.forEach(btn => {
+    btn.addEventListener('click', openPopUp )
+})
+
+let Btns = [closeBtn1,closeBtn2];
+[closeBtn1,closeBtn2].forEach(btn => {
+    btn.addEventListener('click',closePopUp)
+})
+
+function openPopUp() {
+    popUp.classList.add('open')
+    // modalContainer.classList.add('open')
+}
+
+function closePopUp() {
+    popUp.classList.remove('open')
+    // modalContainer.classList.remove('open')
+}
